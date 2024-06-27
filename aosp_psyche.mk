5bd8625bedecc977a64e2fd28f5145cb544192b6
Verevka <verevka86@gmail.com>
2024-06-27 08:40:15 +0000

    psyche: Aospify

--- aosp_psyche.mk
+++ aosp_psyche.mk
@@ -8,13 +8,26 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit Google Pixel Updatable APEX
+TARGET_SHIP_PREBUILT_APEX := true
+$(call inherit-product-if-exists, vendor/pixel-additional/config.mk)
+
+# Device Specific Flags
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Environment Flags
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
 # Inherit from psyche device
 $(call inherit-product, device/xiaomi/psyche/device.mk)
 
-PRODUCT_NAME := lineage_psyche
+PRODUCT_NAME := aosp_psyche
 PRODUCT_DEVICE := psyche
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
